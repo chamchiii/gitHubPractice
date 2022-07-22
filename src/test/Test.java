558b782e20d95c33388ac44f7fece4c2653b9e52
kaido8 <dltmdwls0082@naver.com>
2022-07-22 13:02:39 +0900

    lee seung jin

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("이동원 깃 연결 완료!!!!!");
+		System.out.println("이승진도 깃 연결 완료!!!!");
 	}
 
 }
